befbf723539e1a7c650d11cec0725f710130c524
jain206542 <jain206542@gmail.com>
2022-01-29 04:09:43 +0530

    new changes

--- smartedukart/urls.py
+++ smartedukart/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('',include('mainhome.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
